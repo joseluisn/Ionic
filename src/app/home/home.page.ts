139b3fef242f8f79cc57dd2f78fd2766ae30ed12
DESKTOP-KVQ5F3E\JOSE LUIS NOVOA <jonovoan@poligran.edu.co>
2019-09-05 13:39:35 -0500

    random

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'; 
+import {interval, Subscription} from'rxjs'; 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
-export class HomePage {
+  selector: 'app-home', 
+  templateUrl: 'home.page.html', 
+  styleUrls: ['home.page.scss'], 
+  }) 
 
-  constructor() {}
+  export class HomePage {
+
+  constructor() {} 
+  names;nameActually:String; 
+  namesIngresed:Array<String>; 
+  
+
+//Generate names random 
+setNames(){
+var nam=Math.floor((Math.random()* this.namesIngresed.length)); 
+this.nameActually=this.namesIngresed[nam]; 
+console.log(this.nameActually) 
+}
+
+//Method by add names and show names of array
+addName(){
+this.namesIngresed =this.names.split(",") 
+console.log(this.namesIngresed) 
+this.setNames();
+this.setInterval()
+} 
+//generate names in a determined time
+setInterval(){
+    setInterval(()=>{this.setNames()},2000);
+  }
+
+ngOnInit(){
+if(this.namesIngresed != null){
+this.setInterval()
+}
+this.nameActually="";
+} 
+
 
 }
